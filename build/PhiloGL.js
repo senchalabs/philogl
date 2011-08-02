dd9f266fdff82f54d8ab9abab712d00c8f6cfe7b
philogb <philogb@gmail.com>
2011-08-02 14:55:22 -0700

    License changed.

--- build/PhiloGL.js
+++ build/PhiloGL.js
@@ -1,6 +1,6 @@
 /**
 @preserve
-Copyright (c) 2011 Nicolas Garcia Belmonte (http://philogb.github.com/)
+Copyright (c) 2011 Sencha Labs - Author: Nicolas Garcia Belmonte (http://philogb.github.com/)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
