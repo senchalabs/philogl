83b9027a7e98fed5f7dfd38db405aae28c1292ec
philogb <philogb@gmail.com>
2011-10-10 11:34:01 -0700

    Version 1.3.2
     * Fixes bugs in picking
     * Adds performance optimizations

--- build/PhiloGL.js
+++ build/PhiloGL.js
@@ -187,7 +187,7 @@ PhiloGL.unpack = function(branch) {
 };
 
 //Version
-PhiloGL.version = '1.3.1';
+PhiloGL.version = '1.3.2';
 
 //Holds the 3D context, holds the application
 var gl, app, globalContext = this;
