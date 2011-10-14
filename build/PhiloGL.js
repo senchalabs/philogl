08b8b7817f749244499bbb17305319127be7992f
philogb <philogb@gmail.com>
2011-10-14 11:19:55 -0700

    Version 1.4.0
     * Opera 12 Support
     * Lazy Picking
     * Better Fx api with a delay property
     * Updated docs

--- build/PhiloGL.js
+++ build/PhiloGL.js
@@ -187,7 +187,7 @@ PhiloGL.unpack = function(branch) {
 };
 
 //Version
-PhiloGL.version = '1.3.2';
+PhiloGL.version = '1.4.0';
 
 //Holds the 3D context, holds the application
 var gl, app, globalContext = this;
