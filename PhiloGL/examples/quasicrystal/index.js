c8ba6784bf178a51734b9e3b3e497e9329d5a9a8
philogb <philogb@gmail.com>
2011-10-25 13:43:37 -0700

    Some changes to the example to allow fullscreen.

--- PhiloGL/examples/quasicrystal/index.js
+++ PhiloGL/examples/quasicrystal/index.js
@@ -6,12 +6,47 @@ var mouseY = 0.5;
 var viewX = 600;
 var viewY = 600;
 var c;
+var $ = function(d) { return document.getElementById(d); };
 
 function load() {
+
   if (!PhiloGL.hasWebGL()) {
     alert("Your browser does not support WebGL");
     return;
   }
+
+  $('fullscreen').addEventListener('click', function(e) {
+    var width = window.innerWidth,
+        height = window.innerHeight,
+        canvas = $('c'),
+        style = canvas.style;
+
+    canvas.width = viewX = width;
+    canvas.height = viewY = height;
+
+    style.position = 'absolute';
+    style.top = '0px';
+    style.left = '0px';
+
+    document.body.appendChild(canvas);
+
+    var anchor = document.createElement('a'),
+        astyle = anchor.style;
+    astyle.position = 'absolute';
+    astyle.top = astyle.left = '0px';
+    astyle.color = '#fff';
+    anchor.innerHTML = 'Click here to leave fullscreen';
+    anchor.href = '#';
+    document.body.appendChild(anchor);
+
+    anchor.addEventListener('click', function() {
+      canvas.width = canvas.height = viewX = viewY = 600;
+      canvas.style.position = 'static';
+      $('container').appendChild(canvas);
+      anchor.parentNode.removeChild(anchor);
+    }, false);
+
+  });
 
   PhiloGL('c', {
     program: [{
