e4017098b535d659ce3117c4908a13e2598b17f9
philogb <philogb@gmail.com>
2014-03-30 16:07:35 -0700

    the postprocessing coordinates where not very precise. This fixes this which would have given accumulation errors

--- src/media.js
+++ src/media.js
@@ -8,13 +8,15 @@
   //post process an image by setting it to a texture with a specified fragment
   //and vertex shader.
   Image.postProcess = (function() {
+    //length given a 45 fov angle, and 0.2 distance to camera
+    var length = 0.16568542494923805;
     var plane = new PhiloGL.O3D.Plane({
       type: 'x,y',
-      xlen: 1,
-      ylen: 1,
+      xlen: length,
+      ylen: length,
       offset: 0
     }), camera = new PhiloGL.Camera(45, 1, 0.1, 500, {
-      position: { x: 0, y: 0, z: 1.205 }
+      position: { x: 0, y: 0, z: -0.2 }
     }), scene = new PhiloGL.Scene({}, camera);
 
     return function(opt) {
